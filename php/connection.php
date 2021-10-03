d4a58fcfe0a35065a66d428052a1d5c3077780e6
Yash Sharma <yashs662@gmail.com>
2021-10-04 00:13:30 +0530

    .

--- php/connection.php
+++ php/connection.php
@@ -1,8 +1,13 @@
 <?php
-    $dbhost = "localhost";
-    $dbusername = "root";
-    $dbpassword = "";
-    $dbname = "quiz_master";
+#remove the below lines to connect to your own sql database
+    $dbname = "epiz_29877799_quiz_master";
+    $dbpassword = "oPaemsEhyCAZp3";
+    $dbusername = "epiz_29877799";
+    $dbhost = "sql103.epizy.com";
+    #$dbhost = "localhost";
+    #$dbusername = "root";
+    #$dbpassword = "";
+    #$dbname = "quiz_master";
 
     if (!$con = mysqli_connect($dbhost, $dbusername, $dbpassword, $dbname)) {
         die("Failed to connect.");
